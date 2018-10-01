b8ed028da8e0f00455644f0a0d255a7e9c2b7a6c
af1 <andrew@keepalbanyboring.com>
2018-10-01 15:11:12 -0400

    Update bot.py
    rich embeds

--- bot.py
+++ bot.py
@@ -41,36 +41,45 @@ async def on_message(message):
         return
     # The command !patch return a link with the lastest patch note
     if message.content.startswith('!patch'):
-        await client.send_message(message.channel, 'Last patch notes: https://www.epicgames.com/fortnite/en/news')
+        await client.send_message(message.channel, 'Latest patch notes: https://www.epicgames.com/fortnite/en/news')
     # The command !help explains the one function
     if message.content.startswith('!help'):
-        await client.send_message(message.channel, 'Type \'!verify\'. That\'s it.')
+        await client.send_message(message.channel, 'Set your Discord nickname to be exacly the same as your Epic Games account name. Then type \'!verify\'. The bot looks at your squad K/D for the current season, so if you have no games played yet, the bot can not verify you.')
     # The command !verify return attribute a rank according to the K/D of the user
     if message.content.startswith("!verify"):
         for list in LIST:
             roles = discord.utils.get(message.server.roles, name=list)
         username = '{0.author.display_name}'.format(message)
         ratio = float(get_ratio(username))
+        msgRatio = str(ratio)
         msgVerified = str(VERIFIED)
         print(ratio)
         print("-")
         if ratio == -1.0:
-            msg = "Your IGN and Discord name must be the same.".format(message)
+            msg = "Your Discord name and IGN must be exactly the same. Change your Discord nickname and try again.".format(message)
             await client.send_message(message.channel, msg)
         elif ratio == -2.0:
-            msg = "Looks like Fortnite Tracker servers are down. Please try again later!".format(message)
+            msg = "Data not found. Either Fortnite Tracker is down, or your account is not a PC account.".format(message)
             await client.send_message(message.channel, msg)
         elif ratio == -3.0:
-            msg = "Data not found. Do you have games played in squad mode this season?".format(message)
+            msg = "No stats found for squad mode in the current season. Play some games and try again.".format(message)
             await client.send_message(message.channel, msg)
         elif ratio > 0 and ratio < VERIFIED:
             role = discord.utils.get(message.server.roles, name=LIST[0])
-            msg = ("{0.author.mention} doesn't have a " + msgVerified + " K/D").format(message) + "."
-            await client.send_message(message.channel, msg)
+            embed = discord.Embed(colour=discord.Colour(0x45278e), url="https://github.com/af1/kdFortniteDiscordBot",)
+            embed.set_author(name="Verify " + message.author.name, icon_url=message.author.avatar_url)
+            #embed.set_footer(text="Fortnite Verify Bot", icon_url=client.user.avatar_url)
+            embed.add_field(name=message.author.name + " does not have over a " + msgVerified + " K/D.", value="** **", inline=False)
+            embed.add_field(name="Season 6 Squads K/D: **" + msgRatio + "**", value="** **", inline=True)
+            await client.send_message(message.channel, embed=embed)
         elif ratio >= VERIFIED:
             role = discord.utils.get(message.server.roles, name=LIST[0])
-            msg = ("{0.author.mention} has over a " + msgVerified + " K/D. " + role.name).format(message) + "!"
-            await client.send_message(message.channel, msg)
+            embed = discord.Embed(colour=discord.Colour(0x45278e), url="https://github.com/af1/kdFortniteDiscordBot",)
+            embed.set_author(name="Verify " + message.author.name, icon_url=message.author.avatar_url)
+            #embed.set_footer(text="Fortnite Verify Bot", icon_url=client.user.avatar_url)
+            embed.add_field(name=message.author.name + " has over a " + msgVerified + " K/D. Verified!", value="** **", inline=False)
+            embed.add_field(name="Season 6 Squads K/D: **" + msgRatio + "**", value="** **", inline=True)
+            await client.send_message(message.channel, embed=embed)
             await client.add_roles(message.author, role) 
             
 @client.event
